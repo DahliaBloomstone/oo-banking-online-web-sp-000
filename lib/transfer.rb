8c46276df1468c46038cb0d8be5d43752b7ad100
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-31 01:34:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,8 +19,11 @@ if @sender.balance > @amount && @status == "pending"
   @sender.balance -= @amount
   @receiver.balance += @amount
   @status = "complete"
+else
+  @status = "rejected"
+  return "Transaction rejected. Please check your account balance."
 end
-
+end 
 def reverse_transfer #can reverse a transfer between accounts, can only reverse executed transfers
 end
 
