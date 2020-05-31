411558e1810246ddcbfc073f7ea459df5b35a4ef
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-31 01:22:33 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,10 +9,19 @@ def initialize(name) #initialize a bank account with a name
   @status = "open" #always inits w a status of open
 end
 
-def deposit(amount)
+def deposit(amount) #can deposit money into account
   @balance += amount
+end
+
+def display_balance # can display its balance
+  "Your Balance is $#{@balance}."
+
+def valid? #is valid with an open status and a balance > 0
+  if @status == "open" && @balance > 0
+    true
+  else
+    false
+  end
 end 
-
-
 
 end
