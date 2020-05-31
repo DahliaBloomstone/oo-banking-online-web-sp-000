ad2f343b9ab41b5f86574d933a739f88fbdb7f37
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-31 01:20:34 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,12 +1,12 @@
 class BankAccount
 
 attr_accessor :balance, :status
-attr_reader :name
+attr_reader :name #can't change its name 
 
 def initialize(name) #initialize a bank account with a name
   @name = name
   @balance = 1000 #always inits w a balance of 1000
-  @status = "open" #always inits w a status of open 
+  @status = "open" #always inits w a status of open
 end
 
 end
