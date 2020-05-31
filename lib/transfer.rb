ef83360ff276126a6a9f422df2e30dfd96dae37f
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-31 01:13:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,2 +1,4 @@
+#acts as a space for a transaction between two instances of the bank account cass. Can reject instances if the transfer accounts aren't valid or if sender doesn't have the $. 
 class Transfer
+
 end
