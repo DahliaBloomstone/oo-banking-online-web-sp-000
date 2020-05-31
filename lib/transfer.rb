326d4d59d36069a495a90520ff0350e9de7ed4dd
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-31 02:00:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,6 @@ def execute_transaction #can execute a successful transaction between two accoun
       @receiver.balance += @amount
       @status = "complete"
    else
-     @sender.valid? && receiver.valid? ? true : false
      @status = "rejected" #rejects transfer if sender does not have enough funds
      return "Transaction rejected. Please check your account balance."
    end
