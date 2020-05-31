3f18f61415d01b76a028fe2299953769d7a5b780
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-31 01:32:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,11 +14,11 @@ def valid? #both accounts are valid, calls on sender and receiver #valid? method
 @sender.valid? && receiver.valid? ? true : false
 end
 
-def execute_transaction #can: execute transaction between two accounts, transfer happens only once, rejects if sender doesn't have the funds 
+def execute_transaction #can: execute transaction between two accounts, transfer happens only once, rejects if sender doesn't have the funds
 
 end
 
-def reverse_transfer
+def reverse_transfer #can reverse a transfer between accounts, can only reverse executed transfers 
 end
 
 
