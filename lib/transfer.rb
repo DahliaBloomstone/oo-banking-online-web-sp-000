6e8e6370ab30397cca9e8b3539180c6e1be61eeb
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-31 01:28:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,9 @@
 #acts as a space for a transaction between two instances of the bank account cass. Can reject instances if the transfer accounts aren't valid or if sender doesn't have the $.
 class Transfer
-  attr_accessor :sender, :reciever, :amount, :status, :count
+  attr_accessor :sender, :receiver, :amount, :status, :count
 
 
-  def initialize(sender, reciever, amount)
+  def initialize(sender, receiver, amount)
     @sender = sender
     @reciever = reciever
     @status = "pending"
